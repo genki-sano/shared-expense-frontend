29c71a51c0170a8092a1609627479ae57b3835c9
genki-sano <g.sano.2441@gmail.com>
2026-01-03 20:13:49 +0900

    chore: import周りのルールをeslintに追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,44 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import importPlugin from "eslint-plugin-import";
+import unusedImports from "eslint-plugin-unused-imports";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  {
+    plugins: {
+      import: importPlugin,
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          warnOnUnassignedImports: true,
+          alphabetize: { order: "asc" },
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
+    "public/**",
+    "environment.d.ts",
     "next-env.d.ts",
   ]),
 ]);
